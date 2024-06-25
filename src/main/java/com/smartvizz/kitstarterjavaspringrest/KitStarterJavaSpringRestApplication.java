51008acedd4457f283496f11c35a482171c61323
Rafal Samek <rsamek@datto.com>
2024-06-26 01:12:41 +0200

    Initial commit

---
+++ src/main/java/com/smartvizz/kitstarterjavaspringrest/KitStarterJavaSpringRestApplication.java
@@ -0,0 +1,13 @@
+package com.smartvizz.kitstarterjavaspringrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitStarterJavaSpringRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KitStarterJavaSpringRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smartvizz/kitstarterjavaspringrest/KitStarterJavaSpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartvizz.kitstarterjavaspringrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitStarterJavaSpringRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
